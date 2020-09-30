aeb6d8ffca340a0fd5f4c5648dc9aee133a8331a
Vibhu Manikpuri <65824056+vibhu-exe@users.noreply.github.com>
2020-09-30 23:45:44 +0530

    create code.cpp
    This commit created the hello world project in cpp
    feel free to contribute in open source

---
+++ code.cpp
@@ -0,0 +1,12 @@
+//This code prints 'Hello, World!' on execution
+//This code is written in C++
+
+#include<iostream.h>
+
+int main()
+{
+  clrscr();
+  cout<<"Hello, World!"<<endl;
+  return 0;
+}  
+  
